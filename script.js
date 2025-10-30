ce80738d3e45c9432ef08a72ba70373c929ec486
DarinJSE <presentshimley@gmail.com>
2025-10-30 13:19:52 +0700

    Adding some features

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const addButton = document.querySelector('.task-input');
 const taskList = document.querySelector('.task-list');
 
+const mptMsg = document.createElement('p');
+mptMsg.classList.add('empty-msg');
+mptMsg.textContent = 'You have NO GOALS, loser!';
+taskList.appendChild(mptMsg);
+
 addButton.addEventListener('click', () => {
   const taskText = prompt("What's your intention?");
   if (taskText) {
+
+    if (taskList.contains(mptMsg)) {
+      taskList.removeChild(mptMsg);
+    }
+    
+
     const newTask = document.createElement('div');
     newTask.classList.add('task');
     newTask.innerHTML = `${taskText} <div class="ddl"><span class="done">✔</span><span class="delete">🗑</span></div>`;
@@ -19,6 +30,10 @@ addButton.addEventListener('click', () => {
 
     deleteBtn.addEventListener('click', () => {
       taskList.removeChild(newTask);
+
+      if (taskList.querySelectorAll('.task').length === 0) {
+        taskList.appendChild(mptMsg);
+      }
     });
   }
 });
